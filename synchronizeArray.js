fc3bb5d7e130684a2e210798b48f06cdbb7903e4
章礼平 <LipperZack@gmail.com>
2016-11-23 03:09:21 +0800

    add changedCallBack to do complicated option and use $.extend as default

--- synchronizeArray.js
+++ synchronizeArray.js
@@ -92,6 +92,73 @@ nList=[{
 }]
 
 
+var $ = $ || {
+  	extend: function() {
+		var src, copyIsArray, copy, name, options, clone,
+			target = arguments[0] || {},
+			i = 1,
+			length = arguments.length,
+			deep = false;
+
+		// Handle a deep copy situation
+		if ( typeof target === "boolean" ) {
+			deep = target;
+			target = arguments[1] || {};
+			// skip the boolean and the target
+			i = 2;
+		}
+
+		// Handle case when target is a string or something (possible in deep copy)
+		if ( typeof target !== "object" && !jQuery.isFunction(target) ) {
+			target = {};
+		}
+
+		// extend jQuery itself if only one argument is passed
+		if ( length === i ) {
+			target = this;
+			--i;
+		}
+
+		for ( ; i < length; i++ ) {
+			// Only deal with non-null/undefined values
+			if ( (options = arguments[ i ]) != null ) {
+				// Extend the base object
+				for ( name in options ) {
+					src = target[ name ];
+					copy = options[ name ];
+
+					// Prevent never-ending loop
+					if ( target === copy ) {
+						continue;
+					}
+
+					// Recurse if we're merging plain objects or arrays
+					if ( deep && copy && ( jQuery.isPlainObject(copy) || (copyIsArray = jQuery.isArray(copy)) ) ) {
+						if ( copyIsArray ) {
+							copyIsArray = false;
+							clone = src && jQuery.isArray(src) ? src : [];
+
+						} else {
+							clone = src && jQuery.isPlainObject(src) ? src : {};
+						}
+
+						// Never move original objects, clone them
+						target[ name ] = jQuery.extend( deep, clone, copy );
+
+					// Don't bring in undefined values
+					} else if ( copy !== undefined ) {
+						target[ name ] = copy;
+					}
+				}
+			}
+		}
+
+		// Return the modified object
+		return target;
+	}
+}
+
+
 function mapFromArray(array, prop) {
     var map = {};
     for (var i=0; i < array.length; i++) {
@@ -133,10 +200,16 @@ function getDelta(o, n)  {
 //deleted 2
 //changed 2
 
-function syncArray(oList, nList){
+function syncArray(oList, nList, changedCallBack){
 	var delta = getDelta(oList,nList);
 	delta.changed.forEach(function(c){
-		oList[c.o.__indexForMapFromArray] = nList[c.n.__indexForMapFromArray]
+		var cb = changedCallBack || function(o,n){
+			// default just extend
+			// complated option will do in custom changedCallBack (like isEqual then not to do)
+          	$.extend(o,n);
+		}
+		cb(oList[c.o.__indexForMapFromArray], nList[c.n.__indexForMapFromArray])
+		
 	})
 
 	delta.deleted.sort(function(a,b){
