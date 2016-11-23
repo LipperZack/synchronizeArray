3de6d048db2e7684f813703efd2913310ccae18d
章礼平 <LipperZack@gmail.com>
2016-11-23 11:49:38 +0800

    simple wrap

--- synchronizeArray.js
+++ synchronizeArray.js
@@ -91,137 +91,140 @@ nList=[{
 	}]
 }]
 
+!function(exp){
+	var $ = $ || {};
+	$.extend = $.extend || function() {
+		var src, copyIsArray, copy, name, options, clone,
+			target = arguments[0] || {},
+			i = 1,
+			length = arguments.length,
+			deep = false;
 
-var $ = $ || {};
-$.extend = $.extend || function() {
-	var src, copyIsArray, copy, name, options, clone,
-		target = arguments[0] || {},
-		i = 1,
-		length = arguments.length,
-		deep = false;
+		// Handle a deep copy situation
+		if ( typeof target === "boolean" ) {
+			deep = target;
+			target = arguments[1] || {};
+			// skip the boolean and the target
+			i = 2;
+		}
 
-	// Handle a deep copy situation
-	if ( typeof target === "boolean" ) {
-		deep = target;
-		target = arguments[1] || {};
-		// skip the boolean and the target
-		i = 2;
-	}
+		// Handle case when target is a string or something (possible in deep copy)
+		if ( typeof target !== "object" && !jQuery.isFunction(target) ) {
+			target = {};
+		}
 
-	// Handle case when target is a string or something (possible in deep copy)
-	if ( typeof target !== "object" && !jQuery.isFunction(target) ) {
-		target = {};
-	}
+		// extend jQuery itself if only one argument is passed
+		if ( length === i ) {
+			target = this;
+			--i;
+		}
 
-	// extend jQuery itself if only one argument is passed
-	if ( length === i ) {
-		target = this;
-		--i;
-	}
+		for ( ; i < length; i++ ) {
+			// Only deal with non-null/undefined values
+			if ( (options = arguments[ i ]) != null ) {
+				// Extend the base object
+				for ( name in options ) {
+					src = target[ name ];
+					copy = options[ name ];
 
-	for ( ; i < length; i++ ) {
-		// Only deal with non-null/undefined values
-		if ( (options = arguments[ i ]) != null ) {
-			// Extend the base object
-			for ( name in options ) {
-				src = target[ name ];
-				copy = options[ name ];
-
-				// Prevent never-ending loop
-				if ( target === copy ) {
-					continue;
-				}
-
-				// Recurse if we're merging plain objects or arrays
-				if ( deep && copy && ( jQuery.isPlainObject(copy) || (copyIsArray = jQuery.isArray(copy)) ) ) {
-					if ( copyIsArray ) {
-						copyIsArray = false;
-						clone = src && jQuery.isArray(src) ? src : [];
-
-					} else {
-						clone = src && jQuery.isPlainObject(src) ? src : {};
+					// Prevent never-ending loop
+					if ( target === copy ) {
+						continue;
 					}
 
-					// Never move original objects, clone them
-					target[ name ] = jQuery.extend( deep, clone, copy );
+					// Recurse if we're merging plain objects or arrays
+					if ( deep && copy && ( jQuery.isPlainObject(copy) || (copyIsArray = jQuery.isArray(copy)) ) ) {
+						if ( copyIsArray ) {
+							copyIsArray = false;
+							clone = src && jQuery.isArray(src) ? src : [];
 
-				// Don't bring in undefined values
-				} else if ( copy !== undefined ) {
-					target[ name ] = copy;
+						} else {
+							clone = src && jQuery.isPlainObject(src) ? src : {};
+						}
+
+						// Never move original objects, clone them
+						target[ name ] = jQuery.extend( deep, clone, copy );
+
+					// Don't bring in undefined values
+					} else if ( copy !== undefined ) {
+						target[ name ] = copy;
+					}
 				}
 			}
 		}
+
+		// Return the modified object
+		return target;
 	}
 
-	// Return the modified object
-	return target;
-}
 
 
+	function mapFromArray(array, prop) {
+	    var map = {};
+	    for (var i=0; i < array.length; i++) {
+	    	array[i].__indexForMapFromArray = i;
+	        map[ array[i][prop] ] = array[i];
+	    }
+	    return map;
+	}
 
-function mapFromArray(array, prop) {
-    var map = {};
-    for (var i=0; i < array.length; i++) {
-    	array[i].__indexForMapFromArray = i;
-        map[ array[i][prop] ] = array[i];
-    }
-    return map;
-}
+	function getDelta(o, n)  {
+	    var delta = {
+	        added: [],
+	        deleted: [],
+	        changed: []
+	    };
+	    var mapO = mapFromArray(o, 'id');
+	    var mapN = mapFromArray(n, 'id');    
+	    for (var id in mapO) {
+	        if (!mapN.hasOwnProperty(id)) {
+	            delta.deleted.push(mapO[id]);
+	        } else{
+	            delta.changed.push({
+	            	o: mapO[id],
+	            	n: mapN[id]
+	            });
+	        }
+	    }
 
-function getDelta(o, n)  {
-    var delta = {
-        added: [],
-        deleted: [],
-        changed: []
-    };
-    var mapO = mapFromArray(o, 'id');
-    var mapN = mapFromArray(n, 'id');    
-    for (var id in mapO) {
-        if (!mapN.hasOwnProperty(id)) {
-            delta.deleted.push(mapO[id]);
-        } else{
-            delta.changed.push({
-            	o: mapO[id],
-            	n: mapN[id]
-            });
-        }
-    }
+	    for (var id in mapN) {
+	        if (!mapO.hasOwnProperty(id)) {
+	        	delete mapN[id].__indexForMapFromArray;
+	            delta.added.push( mapN[id] )
+	        }
+	    }
+	    return delta;
+	}
 
-    for (var id in mapN) {
-        if (!mapO.hasOwnProperty(id)) {
-        	delete mapN[id].__indexForMapFromArray;
-            delta.added.push( mapN[id] )
-        }
-    }
-    return delta;
-}
+	//added 1 
+	//deleted 2
+	//changed 2
 
-//added 1 
-//deleted 2
-//changed 2
+	function syncArray(oList, nList, changedCallBack){
+		var delta = getDelta(oList,nList);
+		delta.changed.forEach(function(c){
+			var cb = changedCallBack || function(o,n){
+				// default just extend
+				// complated option will do in custom changedCallBack (like isEqual then not to do)
+				delete o.__indexForMapFromArray;
+				delete n.__indexForMapFromArray;
+	          	$.extend(o,n);
+			}
+			cb(oList[c.o.__indexForMapFromArray], nList[c.n.__indexForMapFromArray])
+			
+		})
 
-function syncArray(oList, nList, changedCallBack){
-	var delta = getDelta(oList,nList);
-	delta.changed.forEach(function(c){
-		var cb = changedCallBack || function(o,n){
-			// default just extend
-			// complated option will do in custom changedCallBack (like isEqual then not to do)
-			delete o.__indexForMapFromArray;
-			delete n.__indexForMapFromArray;
-          	$.extend(o,n);
-		}
-		cb(oList[c.o.__indexForMapFromArray], nList[c.n.__indexForMapFromArray])
-		
-	})
+		delta.deleted.sort(function(a,b){
+			return b.__indexForMapFromArray - a.__indexForMapFromArray
+		})
+		delta.deleted.forEach(function(d){
+			oList.splice(d.__indexForMapFromArray, 1)
+		})
+		oList.push.apply(oList, delta.added);
+	}
 
-	delta.deleted.sort(function(a,b){
-		return b.__indexForMapFromArray - a.__indexForMapFromArray
-	})
-	delta.deleted.forEach(function(d){
-		oList.splice(d.__indexForMapFromArray, 1)
-	})
-	oList.push.apply(oList, delta.added);
-}
+	exp.syncArray = syncArray
+}(window)
 
 syncArray(oList, nList);
 
