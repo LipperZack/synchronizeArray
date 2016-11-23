d288b5063084f0d3de3acddaf59f77279899b2dc
章礼平 <LipperZack@gmail.com>
2016-11-23 17:08:00 +0800

    use jquery-extend-without-jquery from http://codegists.com/snippet/javascript/jquery-extend-without-jqueryjs_cfv1984_javascript

--- synchronizeArray.js
+++ synchronizeArray.js
@@ -110,68 +110,51 @@ nList = [{
 ! function(exp) {
     var $ = $ || {};
     $.extend = $.extend || function() {
-        var src, copyIsArray, copy, name, options, clone,
-            target = arguments[0] || {},
+
+        function isFunction(fn) {
+            return typeof(fn) === "function" && fn.constructor === Function }
+
+        function isArray(ar) {
+            return ar instanceof Array }
+
+        function isPlainObject(obj) {
+            return typeof obj == 'object' && obj.constructor == Object }
+
+        var options, name, src, copy, copyIsArray, clone, target = arguments[0] || {},
             i = 1,
             length = arguments.length,
             deep = false;
-
-        // Handle a deep copy situation
         if (typeof target === "boolean") {
             deep = target;
-            target = arguments[1] || {};
-            // skip the boolean and the target
-            i = 2;
+            target = arguments[i] || {};
+            i++;
         }
-
-        // Handle case when target is a string or something (possible in deep copy)
-        if (typeof target !== "object" && !jQuery.isFunction(target)) {
-            target = {};
-        }
-
-        // extend jQuery itself if only one argument is passed
-        if (length === i) {
+        if (typeof target !== "object" && !isFunction(target)) target = {};
+        if (i === length) {
             target = this;
-            --i;
+            i--;
         }
-
         for (; i < length; i++) {
-            // Only deal with non-null/undefined values
             if ((options = arguments[i]) != null) {
-                // Extend the base object
                 for (name in options) {
                     src = target[name];
                     copy = options[name];
+                    if (target === copy) continue;
 
-                    // Prevent never-ending loop
-                    if (target === copy) {
-                        continue;
-                    }
-
-                    // Recurse if we're merging plain objects or arrays
-                    if (deep && copy && (jQuery.isPlainObject(copy) || (copyIsArray = jQuery.isArray(copy)))) {
-                        if (copyIsArray) {
+                    if (deep && copy && (isPlainObject(copy) || (copyIsArray = isArray(copy)))) {
+                        if (!copyIsArray) {
                             copyIsArray = false;
-                            clone = src && jQuery.isArray(src) ? src : [];
+                            clone = src && isArray(src) ? src : [];
+                        } else clone = src && isPlainObject(src) ? src : {};
 
-                        } else {
-                            clone = src && jQuery.isPlainObject(src) ? src : {};
-                        }
-
-                        // Never move original objects, clone them
-                        target[name] = jQuery.extend(deep, clone, copy);
-
-                        // Don't bring in undefined values
-                    } else if (copy !== undefined) {
-                        target[name] = copy;
-                    }
+                        target[name] = extend(deep, clone, copy);
+                    } else if (copy !== undefined) target[name] = copy;
                 }
             }
         }
 
-        // Return the modified object
         return target;
-    }
+    };
 
 
 
@@ -246,9 +229,9 @@ nList = [{
 syncArray(oList, nList);
 // custom callback
 // syncArray(oList, nList, 'id', function(o, n){
-// 	delete o.__indexForMapFromArray;
-// 	delete n.__indexForMapFromArray;
-//   	syncArray(o.innerArray, n.innerArray, 'innerId');
+//  delete o.__indexForMapFromArray;
+//  delete n.__indexForMapFromArray;
+//      syncArray(o.innerArray, n.innerArray, 'innerId');
 // });
 
 oList;
